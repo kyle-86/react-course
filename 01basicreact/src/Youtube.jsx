5dea897c322b7474d0324193f4e2135fbbdbc294
Kyle H <kjhunter86@gmail.com>
2024-05-18 12:28:28 +1000

    Completed File structure Of React and making First component

---
+++ 01basicreact/src/Youtube.jsx
@@ -0,0 +1,12 @@
+function Youtube(params) {
+    return(
+        <>
+        <h1> Youtube react H1</h1>
+        <h2> Youtube react H2</h2>
+        <h3> Youtube react H3</h3>
+        <h4> Youtube react H4</h4>
+        </>
+    )
+}
+
+export default Youtube
