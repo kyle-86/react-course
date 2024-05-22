52a81f5da023d9ecabe5aae329b14d5977c9783b
Kyle H <kjhunter86@gmail.com>
2024-05-22 15:30:16 +1000

    ReactRouting(Artificial routing) using react-router-dom, simple portfolio application

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' bg-emerald-600 text-fuchsia-600 text-3xl'> Testing tailspin</h1>
+    </>
+  )
+}
+
+export default App
